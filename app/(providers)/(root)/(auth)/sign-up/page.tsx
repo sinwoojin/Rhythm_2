c8515d9e3823e0c42286757eae73c7e026ccb172
sinwoojin <slow3993@gmail.com>
2024-10-11 10:46:55 +0900

    로그인 인풋 뼈대 잡기

--- app/(providers)/(root)/(auth)/sign-up/page.tsx
+++ app/(providers)/(root)/(auth)/sign-up/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client";
 
 function SignUpPage() {
-  return (
-    <div>page</div>
-  )
+	return (
+		<form>
+			<label htmlFor="userName">유저 이름</label>
+			<input type="text" id="userName" />
+
+			<label htmlFor="email">이메일</label>
+			<input type="text" id="email" />
+
+			<label htmlFor="password">비밀번호</label>
+			<input type="password" id="password" />
+
+			<label htmlFor="passwordFirm"></label>
+		</form>
+	);
 }
 
-export default SignUpPage
+export default SignUpPage;
