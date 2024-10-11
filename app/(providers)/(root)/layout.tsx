953f6496076a24c55f7a5a0e8667db0054c19cdd
lee <leeganghee4954@naver.com>
2024-10-11 16:17:58 +0900

    feat: 레이아웃 완성

--- app/(providers)/(root)/layout.tsx
+++ app/(providers)/(root)/layout.tsx
@@ -1,11 +1,15 @@
 import React, { PropsWithChildren } from "react";
 import Header from "./_components/_RootLayout/_Header/Header";
+import MusicPlayer from "./_components/_RootLayout/_MusicPlayer/MusicPlayer";
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div>
-      <Header />
-      {children}
+    <div className="flex flex-col h-screen">
+      <div className="flex h-full">
+        <Header />
+        {children}
+      </div>
+      <MusicPlayer />
     </div>
   );
 }
