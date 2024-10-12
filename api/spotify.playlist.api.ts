f8a3e813a33d62e02aaed14069d8d1e2aac9594f
shinhyeanseong <ahddud89@gmail.com>
2024-10-12 19:10:29 +0900

    feat: getCategoryPlaylist

--- api/spotify.playlist.api.ts
+++ api/spotify.playlist.api.ts
@@ -33,7 +33,7 @@ const getPlaylists = async (playlistId: string) => {
 
 /**
  * 플레이 리스트 만들기(userId 필요)
- * @param playlistId
+ * @param userId
  */
 const createPlaylists = async (userId: string) => {
   try {
@@ -57,8 +57,62 @@ const createPlaylists = async (userId: string) => {
     console.error("Error fetching album information:", error);
   }
 };
+/**
+ * 플레이 리스트 수정하기(userId 필요)
+ * @param userId
+ */
+const editPlaylist = async (userId: string) => {
+  try {
+    const accessToken = await getAccessToken(); // 액세스 토큰을 비동기로 가져옴
+    if (!accessToken) {
+      throw new Error("Access token is required");
+    }
+
+    const response = await baseURL.spotifyPlayListAPI.put(
+      `${userId}/playlists`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    // 응답 데이터 출력
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching album information:", error);
+  }
+};
+/**
+ * 카테고리별 플레이 리스트 가져오기(category_id 필요)
+ * @param category_id
+ */
+const getCategoryPlaylist = async (category_id: string) => {
+  try {
+    const accessToken = await getAccessToken(); // 액세스 토큰을 비동기로 가져옴
+    if (!accessToken) {
+      throw new Error("Access token is required");
+    }
+
+    const response = await baseURL.spotifyPlayListAPI.get(
+      `browse/categories/${category_id}/playlists`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    // 응답 데이터 출력
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error fetching album information:", error);
+  }
+};
 
 export const PlaylistAPI = {
   getPlaylists,
   createPlaylists,
+  editPlaylist,
+  getCategoryPlaylist,
 };
