4535f6f07345ace58fffe6a9803e55f2c8b5e70a
Mye247 <louemc1@gmail.com>
2024-10-13 18:30:50 +0900

    modify: Category UI 간단히 수정

--- app/(providers)/(root)/playlists/page.tsx
+++ app/(providers)/(root)/playlists/page.tsx
@@ -4,49 +4,49 @@ import Page from "../_components/_Page/Page";
 function PlaylistsPage() {
   return (
     <Page title="User Rhythm!">
-      <section className="grid grid-cols-2 gap-y-5 h-[500px]">
+      <section className="grid grid-cols-2 gap-y-5 h-[467px]">
         {/* 각 블록을 클릭하면 해당 카테고리의 플레이리스트가 모여서 보이게 (이미지로 설정)*/}
 
         <Link href={"/playlists/k-pop"}>
           <div className="">
-            <p className="mb-2">K-POP</p>
+            <p className="mb-1">K-POP</p>
             <img
               src=""
               alt=""
-              className="w-[300px] h-[180px] bg-slate-700 rounded-lg mb-10 "
+              className="w-[300px] h-[180px] bg-slate-700 rounded-lg "
             />
           </div>
         </Link>
 
         <Link href={"/playlists/k-pop"}>
           <div className="">
-            <p className="mb-2">J-POP</p>
+            <p className="mb-1">J-POP</p>
             <img
               src=""
               alt=""
-              className="w-[300px] h-[180px] bg-slate-700 rounded-lg mb-10 "
+              className="w-[300px] h-[180px] bg-slate-700 rounded-lg  "
             />
           </div>
         </Link>
 
         <Link href={"/playlists/k-pop"}>
           <div className="">
-            <p className="mb-2">HIP-HOP</p>
+            <p className="mb-1">HIP-HOP</p>
             <img
               src=""
               alt=""
-              className="w-[300px] h-[180px] bg-slate-700 rounded-lg mb-10 "
+              className="w-[300px] h-[180px] bg-slate-700 rounded-lg "
             />
           </div>
         </Link>
 
         <Link href={"/playlists/k-pop"}>
           <div className="">
-            <p className="mb-2">BAND</p>
+            <p className="mb-1">BAND</p>
             <img
               src=""
               alt=""
-              className="w-[300px] h-[180px] bg-slate-700 rounded-lg mb-10 "
+              className="w-[300px] h-[180px] bg-slate-700 rounded-lg "
             />
           </div>
         </Link>
