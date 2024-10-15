5567f7ec29d3b82e0ad97cd5108b44f3a52dc00f
sinwoojin <slow3993@gmail.com>
2024-10-15 13:15:20 +0900

    chore:필요없는 api파일 삭제

--- api/spotifyApi.ts
+++ api/spotifyApi.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import getUserApi from "./getUser";
 import { AlbumAPI } from "./spotifyAlbumApi";
 import { PlaylistAPI } from "./spotifyPlaylistApi";
-import { ProfileAPI } from "./spotifyProfileApi";
 
 const spotifyAPI = axios.create({ baseURL: "https://api.spotify.com/v1/" });
 const spotifyUserAPI = axios.create({
@@ -17,6 +16,5 @@ export const baseURL = {
 export const api = {
 	AlbumAPI,
 	PlaylistAPI,
-	ProfileAPI,
 	getUserApi,
 };
