47c336b3b817163a626654f9614784fb41c8291c
sinwoojin <slow3993@gmail.com>
2024-10-15 15:57:27 +0900

    chore:차트 페이지 레이아웃 잡기

---
+++ app/(providers)/(root)/chart/page.tsx
@@ -0,0 +1,12 @@
+import Page from "../_components/_Page/Page";
+import Chart from "./_components/Chart/Chart";
+
+function ChartPage() {
+	return (
+		<Page title="차트">
+			<Chart title="오늘의 top 100" />
+		</Page>
+	);
+}
+
+export default ChartPage;
