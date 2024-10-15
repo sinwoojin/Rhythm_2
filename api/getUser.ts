93afb624e1e98de56bd310abf2bfb3ce39ab7d7a
sinwoojin <slow3993@gmail.com>
2024-10-15 13:21:44 +0900

    feat: 스포티파이로 로그인시 정보 가져오기

--- api/getUser.ts
+++ api/getUser.ts
@@ -1,13 +1,21 @@
 import { supabase } from "@/supabase/client";
 
-const getUser = async () => {
+const getUserSupabase = async () => {
 	const response = await supabase.auth.getUser();
 	const data = response.data.user;
 	return data;
 };
 
+const getUserSpotify = async () => {
+	const response = await supabase.auth.getUser();
+	const data = response.data.user?.user_metadata;
+
+	return data;
+};
+
 const getUserApi = {
-	getUser,
+	getUserSupabase,
+	getUserSpotify,
 };
 
 export default getUserApi;
