f67204540b4e451b69ab614f7e4f8dfccadc7500
sinwoojin <slow3993@gmail.com>
2024-10-21 10:40:02 +0900

    chore/모달 여는거 바꾸기

--- app/(providers)/(root)/profiles/_components/Modals/EditModal.tsx
+++ app/(providers)/(root)/profiles/_components/Modals/EditModal.tsx
@@ -3,30 +3,21 @@
 import Input from '@/components/Input';
 import { Database } from '@/database.types';
 import { supabase } from '@/supabase/client';
+import { useModalStore } from '@/zustand/modalStore';
 import { nanoid } from 'nanoid';
 import { ComponentProps, useEffect, useState } from 'react';
 
-interface EditModalProps {
+type EditModalProps = {
   id: string;
-  modal: boolean;
-  onClose: () => void;
-}
+};
 
-function EditModal(props: EditModalProps) {
-  // props
-  const id = props.id;
-  const modal = props.modal;
-  const onClose = props.onClose;
-
+function EditModal({ id }: EditModalProps) {
   // table에 들어있는 정보 가져오기, 지정하기
   const [userName, setUserName] = useState('');
   const [content, setContent] = useState('');
   const [image, setImage] = useState<File | null>(null);
 
-  // 바깥영역 클릭시 나가짐
-  const handleToggleModal = () => {
-    onClose();
-  };
+  const closeModal = useModalStore((state) => state.closeModal);
 
   // 이미지 정보 가져오기
   const handleChangeFileInput: ComponentProps<'input'>['onChange'] = (e) => {
@@ -66,7 +57,7 @@ function EditModal(props: EditModalProps) {
       return alert('프로필 수정에 실패했습니다!...');
     } else {
       alert('프로필 수정에 성공했습니다!');
-      onClose();
+      closeModal;
     }
   };
 
@@ -84,70 +75,45 @@ function EditModal(props: EditModalProps) {
       }
     })();
   }, [id]);
-  console.log('image', image);
 
   return (
-    <>
-      {/* true때 보임 */}
-      {modal && (
-        <main
-          className="bg-white/10 flex items-center justify-center fixed top-0 left-0 right-0 bottom-0 z-20"
-          onClick={handleToggleModal}
+    <div>
+      <div
+        className="absolute top-[50%] left-[50%] w-[500px] h-[530px] bg-[#121212] -translate-x-[50%] -translate-y-[50%] rounded-2xl text-white"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2 className="text-center mt-10 font-semibold text-3xl">로그인</h2>
+        <form
+          className="flex items-center justify-center flex-col gap-y-3"
+          onSubmit={handleSubmitModifyDeal}
         >
-          <div
-            className="absolute top-[50%] left-[50%] w-[500px] h-[530px] bg-[#121212] -translate-x-[50%] -translate-y-[50%] rounded-2xl text-white"
-            onClick={(e) => e.stopPropagation()}
-          >
-            <h2 className="text-center mt-10 font-semibold text-3xl mb-4">
-              프로필 수정
-            </h2>
-            <form
-              className="flex items-center justify-center flex-col gap-y-3"
-              onSubmit={handleSubmitModifyDeal}
-            >
-              <div className="flex flex-col w-2/3 text-center gap-y-3">
-                <input
-                  className="w-0 h-0 p-0 overflow-hidden border border-0 absolute"
-                  type="file"
-                  id="profileImg"
-                  onChange={handleChangeFileInput}
-                />
-                <label htmlFor="profileImg">
-                  <span className="pb-1 border-b cursor-pointer font-bold">
-                    사진 변경
-                  </span>
-                </label>
-                <Input value={image?.name} disabled />
-              </div>
+          <label htmlFor="img">프로필 이미지</label>
+          <input type="file" id="img" onChange={handleChangeFileInput} />
 
-              <label htmlFor="userName">유저 이름</label>
-              <Input
-                className="w-2/3"
-                size={'large'}
-                padding={'md'}
-                id="userName"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-              />
+          <label htmlFor="userName">유저 이름</label>
+          <Input
+            type="text"
+            id="userName"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            className="text-black"
+          />
 
-              <label htmlFor="content">소개글</label>
-              <Input
-                className="w-2/3"
-                size={'large'}
-                padding={'md'}
-                id="content"
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
+          <label htmlFor="content">소개글</label>
+          <Input
+            type="text"
+            id="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="text-black"
+          />
 
-              <button className="border border-white bg-[#121212] text-white w-[400px] h-[60px] mt-5 hover:-translate-y-2 transition-all">
-                정보 수정하기
-              </button>
-            </form>
-          </div>
-        </main>
-      )}
-    </>
+          <button className="border border-white bg-[#121212] text-white w-[400px] h-[60px] mt-5 hover:-translate-y-2 transition-all">
+            정보 수정하기
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
