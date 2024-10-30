638f60c174053f9c9e16253289643840de034c52
shinhyeanseong <ahddud89@gmail.com>
2024-10-30 14:29:16 +0900

    chore/플레이 리스트에 사진이 없을 경우 에러나는 오류 수정

--- app/(providers)/(root)/_components/MyPlaylists/MyPlaylists.tsx
+++ app/(providers)/(root)/_components/MyPlaylists/MyPlaylists.tsx
@@ -31,53 +31,60 @@ function MyPlaylists({ location }: MyPlaylistsProps) {
   if (!myPlaylists) return;
   if (location === 'my')
     return (
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {myPlaylists?.map((myPlaylist) => (
-          <li
-            key={myPlaylist.id}
-            className="bg-[#1c1c1c] rounded-lg overflow-hidden hover:scale-105 transition-transform"
-          >
-            <Link href={`/playlists/${myPlaylist.id}`} className="block">
-              <div className="relative w-full h-48">
-                {myPlaylist.images && myPlaylist.images.length > 0 ? (
-                  <img
-                    src={myPlaylist.images[0].url}
-                    alt={myPlaylist.name}
-                    className="object-contain w-full h-full"
-                  />
-                ) : (
-                  <div className="w-full h-full bg-gray-700 flex justify-center items-center">
-                    <MdMusicOff className="w-10 h-10 text-white/70" />
-                  </div>
-                )}
-              </div>
-              <div className="p-4 text-white">
-                <h3 className="text-lg font-semibold truncate">
-                  {myPlaylist.name}
-                </h3>
-                <p className="text-sm text-gray-400">
-                  {myPlaylist.owner.display_name}
-                </p>
-                <p className="text-sm text-gray-500 mt-1">
-                  트랙 수: {myPlaylist.tracks.total}
-                </p>
-              </div>
-            </Link>
-            <div className="flex items-center justify-between px-4 py-2 bg-[#111]">
-              {currentUser && currentUser.spotifyId ? (
-                <DeletePlayListButton playlistId={myPlaylist.id} />
-              ) : null}
-              <button
-                aria-label="옵션 버튼"
-                className="text-xl text-white/50 hover:text-white"
+      <div>
+        {myPlaylists.length > 0 ? (
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {myPlaylists?.map((myPlaylist) => (
+              <li
+                key={myPlaylist.id}
+                className="bg-[#1c1c1c] rounded-lg overflow-hidden hover:scale-105 transition-transform"
               >
-                <SlOptions />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+                <Link href={`/playlists/${myPlaylist.id}`} className="block">
+                  <div className="relative w-full h-48">
+                    {myPlaylist.images && myPlaylist.images.length > 0 ? (
+                      <img
+                        src={myPlaylist.images[0].url}
+                        alt={myPlaylist.name}
+                        className="object-contain w-full h-full"
+                      />
+                    ) : (
+                      <div className="w-full h-full bg-gray-700 flex justify-center items-center">
+                        <MdMusicOff className="w-10 h-10 text-white/70" />
+                      </div>
+                    )}
+                  </div>
+                  <div className="p-4 text-white">
+                    <h3 className="text-lg font-semibold truncate">
+                      {myPlaylist.name}
+                    </h3>
+                    <p className="text-sm text-gray-400">
+                      {myPlaylist.owner.display_name}
+                    </p>
+                    <p className="text-sm text-gray-500 mt-1">
+                      트랙 수: {myPlaylist.tracks.total}
+                    </p>
+                  </div>
+                </Link>
+                <div className="flex items-center justify-between px-4 py-2 bg-[#111]">
+                  {currentUser && currentUser.spotifyId ? (
+                    <DeletePlayListButton playlistId={myPlaylist.id} />
+                  ) : null}
+                  <button
+                    aria-label="옵션 버튼"
+                    className="text-xl text-white/50 hover:text-white"
+                  >
+                    <SlOptions />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <Page>플레이 리스트가 존재하지 않습니다</Page>
+        )}
+      </div>
     );
+
   if (location === 'profile')
     return (
       <div className="relative max-w-full">
