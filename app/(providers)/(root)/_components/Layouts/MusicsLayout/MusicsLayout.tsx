9f2abb6403b42f366f4a083f9e0fac93d41aed26
sinwoojin <slow3993@gmail.com>
2024-10-31 16:29:53 +0900

    feat/제목 긴거 슬라이드 보여주기

--- app/(providers)/(root)/_components/Layouts/MusicsLayout/MusicsLayout.tsx
+++ app/(providers)/(root)/_components/Layouts/MusicsLayout/MusicsLayout.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { Track } from '@/schema/type';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -28,14 +29,25 @@ function MusicsLayout({ tracks, title }: ChartListProps) {
                   <Link href={`/tracks/${track.id}`}>
                     <div className="w-full aspect-square overflow-hidden mb-2">
                       <img
+                        id="overFlowSlide"
                         className="w-full h-full object-cover"
                         src={track.album.images[0]?.url || '/default-image.jpg'}
                         alt="이미지"
                       />
                     </div>
-                    <p className="text-xl font-semibold line-clamp-1 w-[calc(90%)]">
-                      {track.name}
-                    </p>
+                    <motion.div className="overflow-hidden w-full group">
+                      <motion.p
+                        whileHover={track.name.length > 18 ? { x: -100 } : {}} // 15자 이상일 때만 왼쪽으로 이동
+                        animate={{ x: 0 }} // 호버가 끝나면 원래 위치로 돌아옴
+                        transition={{
+                          type: 'tween',
+                          duration: 3,
+                        }}
+                        className="text-xl font-semibold whitespace-nowrap "
+                      >
+                        {track.name}
+                      </motion.p>
+                    </motion.div>
                   </Link>
                 </SwiperSlide>
               ))}
