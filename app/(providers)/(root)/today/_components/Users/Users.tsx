487281e6414eab2b49ef6a2aa5e29ced7fa10d57
sinwoojin <slow3993@gmail.com>
2024-11-01 09:59:30 +0900

    feat/today페이지에 사용자 정보 보여주기

--- app/(providers)/(root)/today/_components/Users/Users.tsx
+++ app/(providers)/(root)/today/_components/Users/Users.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { api } from '@/api/spotifyApi';
+import { baseURL } from '@/config/config';
 import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 function Users() {
   const { data: users } = useQuery({
@@ -11,16 +13,41 @@ function Users() {
   });
 
   return (
-    <div>
-      <ul>
-        {users?.map((user) => (
-          <li key={user.id}>
-            <Link href={`profiles/${user.id}`}>
-              <p>{user.userName}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="mt-20">
+      <h3 className="text-2xl font-bold mb-6">사용자 목록</h3>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={5.5}
+        loop={false}
+        className="overflow-hidden"
+      >
+        <ul className="w-full flex gap-x-7">
+          {users?.map((user) => (
+            <SwiperSlide
+              key={user.id}
+              className="w-[200px] p-5 bg-white/20 rounded-3xl"
+            >
+              <Link href={`/profiles/${user.id}`}>
+                <div className="relative group w-full aspect-square rounded-md p-5">
+                  <img
+                    src={baseURL + user.imgUrl}
+                    alt="플레이 리스트 커버 이미지"
+                    className="w-full h-full object-cover rounded-full group-hover:brightness-50 "
+                  />
+                </div>
+                <div id="content-box" className="border-t border-white py-3">
+                  <p className="font-semibold text-lg truncate ">
+                    {user.userName}
+                  </p>
+                  <p className="text-sm text-white/80 truncate ">
+                    {user.content}
+                  </p>
+                </div>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </ul>
+      </Swiper>
     </div>
   );
 }
